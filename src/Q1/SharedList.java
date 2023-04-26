d1937ec79c09f30fb9b55f190358d0e340d9708c
Sina <sadeghesfahani.sina@gmail.com>
2023-04-26 08:48:30 +0430

    Implement shared list

---
+++ src/Q1/SharedList.java
@@ -0,0 +1,28 @@
+package Q1;
+
+import java.util.ArrayList;
+
+public class SharedList {
+    private final ArrayList<Integer> list = new ArrayList<>();
+    private boolean isOddThreadTurn = false;
+    public synchronized void addEven(int evenNumber) throws InterruptedException {
+        while (isOddThreadTurn) {
+            wait();
+        }
+        list.add(evenNumber);
+        isOddThreadTurn = true;
+        notify();
+    }
+    public synchronized void addOdd(int oddNumber) throws InterruptedException {
+        while (!isOddThreadTurn) {
+            wait();
+        }
+        list.add(oddNumber);
+        isOddThreadTurn = false;
+        notify();
+    }
+
+    public ArrayList<Integer> getList() {
+        return list;
+    }
+}
