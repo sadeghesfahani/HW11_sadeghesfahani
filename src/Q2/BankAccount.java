c4eeac22d0374e87380de05c6057a875944c9d56
Sina <sadeghesfahani.sina@gmail.com>
2023-04-26 08:49:59 +0430

    Implement Bank account

---
+++ src/Q2/BankAccount.java
@@ -0,0 +1,40 @@
+package Q2;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class BankAccount {
+    private double balance;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        lock.writeLock().lock();
+        try {
+            balance += amount;
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    public void withdraw(double amount) {
+        lock.writeLock().lock();
+        try {
+            balance -= amount;
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    public double getBalance() {
+        lock.readLock().lock();
+        try {
+            return balance;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+}
